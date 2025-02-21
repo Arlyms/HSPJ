bdd2b5c777ab2b2297ac88a57b2473c2690d46d6
Arlyms <nathan.lesueur01400@gmail.com>
2025-02-21 13:37:07 +0100

    ajout d'un component card

---
+++ src/components/Card.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="card">
+        <div class="card-image">
+
+        </div>
+        <div class="card-content">
+            <div class="content">
+                <h3 class="tittle">Tittle</h3>
+                <p class="description">Description</p>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
